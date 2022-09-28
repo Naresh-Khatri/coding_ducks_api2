9468f70db919ad4acf0c1a8b235639476012c0fd
naresh-khatri <naresh.khatri2345@gmail.com>
2022-09-28 16:51:12 +0530

    create a funciton to update userinfo

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export const getUsers = async (req: Request, res: Response) => {
@@ -16,6 +16,8 @@ export const createUser = async (req: Request, res: Response) => {
         })
         res.json(newUser)
     } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002')
+            return res.status(400).json({ code: 69, message: 'User already exists' })
         res.status(404).json({ message: 'somethings wrong' })
         console.log(error)
     }
@@ -24,6 +26,16 @@ export const getUser = async (req: Request, res: Response) => {
     try {
         const user = await prisma.user.findFirst({ where: { googleUID: req.params.id } })
         res.json(user || [])
+    } catch (err) {
+        res.status(404).json({ message: 'somethings wrong' })
+        console.log(err)
+    }
+}
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        console.log(req.body)
+        const user = await prisma.user.update({ where: { googleUID: req.body.googleUID }, data: { ...req.body } })
+        res.json(user)
     } catch (err) {
         res.status(404).json({ message: 'somethings wrong' })
         console.log(err)
