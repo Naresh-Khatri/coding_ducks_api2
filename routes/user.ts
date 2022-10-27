6fbcdf05a3f2065a8e7d8fa21b12a0c2770c30f2
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-27 16:51:12 +0530

    add new route to update user info

--- routes/user.ts
+++ routes/user.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUser);
+router.patch('/', userController.updateUser);
 router.post('/checkUsername', userController.checkUsername);
 router.post('/', userController.createUser);
 
