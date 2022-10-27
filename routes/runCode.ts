7f81b23ffe3ed2aa8f33f6542b28199812e75d08
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-27 16:51:12 +0530

    beautify runCode function ✨✨

--- routes/runCode.ts
+++ routes/runCode.ts
@@ -7,26 +7,22 @@ const router = Router();
 router.post('/', async (req: Request, res: Response) => {
     const { code, lang } = req.body;
     const output = await runCode(code, lang)
+    console.log(output)
     res.json(output)
 });
 
 
 const runCode = (code: string, lang: string) => {
-    if (lang === 'c') {
+    if (lang === 'c')
         return c.runSource(code)
-    }
-    if (lang === 'cpp') {
+    if (lang === 'cpp')
         return cpp.runSource(code)
-    }
-    if (lang === 'py') {
+    if (lang === 'py')
         return python.runSource(code)
-    }
-    if (lang === 'java') {
+    if (lang === 'java')
         return java.runSource(code)
-    }
-    if (lang === 'js') {
+    if (lang === 'js')
         return node.runSource(code)
-    }
 }
 
 
