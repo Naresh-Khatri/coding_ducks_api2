d77625139a59dd8ba7eac332404e6d35758a4665
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-31 16:51:12 +0530

    use auth middleware and create additional routes

--- routes/problems.ts
+++ routes/problems.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
 import * as problemsController from '../controllers/problems'
+import { checkIfAuthenticated } from '../middlewares/auth-middleware'
 
 const router = Router()
 
 //TODO: add auth
 router.get('/', problemsController.getAllProblems)
+router.get('/examProblems/:examId', problemsController.getExamProblems)
+router.get('/:problemId', [checkIfAuthenticated], problemsController.getProblem)
+router.post('/', [checkIfAuthenticated], problemsController.createProblem)
+router.patch('/:problemId', [checkIfAuthenticated], problemsController.updateProblem)
+router.delete('/:problemId', [checkIfAuthenticated], problemsController.deleteProblem)
 
 export default router
+
