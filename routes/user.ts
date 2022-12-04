e2736422c5cfa09d3feec36fdf0c90b7bcf93704
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-04 16:51:12 +0530

    add routes to get user from username, follow and unfollow a user

--- routes/user.ts
+++ routes/user.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import * as userController from "../controllers/user";
+import { checkIfAuthenticated } from "../middlewares/auth-middleware";
 
 const router = Router();
 
 router.get('/', userController.getUsers);
-router.get('/:id', userController.getUser);
+router.get('/:gid', userController.getUser);
+router.get('/username/:username', userController.getUserUsingUsername);
 router.patch('/', userController.updateUser);
 router.post('/checkUsername', userController.checkUsername);
 router.post('/', userController.createUser);
+router.post('/follow', [checkIfAuthenticated], userController.followUser);
+// router.get('/getFollowDetails/:username', [checkIfAuthenticated], userController.getFollowDetails);
+router.post('/unfollow', [checkIfAuthenticated], userController.unfollowUser);
 
 
 export default router
