75a6076b8578fc8b61991aca612d93492a8c76f0
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-11 22:18:16 +0530

    save user submissions in database

--- routes/runCode.ts
+++ routes/runCode.ts
@@ -1,17 +1,33 @@
 import { Request, Response, Router } from "express";
 import { cpp, c, python, java, node } from "compile-run";
 import { PrismaClient } from '@prisma/client'
+import { checkIfAuthenticated } from "../middlewares/auth-middleware";
 const prisma = new PrismaClient()
 
 const router = Router();
 
+interface Result {
+    results: Array<{ input: string, output: string, actualOutput: string, result: any, isCorrect: boolean }>,
+    passedCount: number,
+    totalCount: number,
+    isCorrect: boolean
+}
 
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', [checkIfAuthenticated], async (req: Request, res: Response) => {
     const { code, lang, problemId } = req.body;
-    console.log(req.body)
-    const result = await evaluateResult(problemId, code, lang)
+    // console.log(req.body)
+    try {
 
-    res.json(result)
+
+        const result = await evaluateResult(problemId, code, lang) as Result
+        if (req.body.submit)
+            await saveInDB(result, req)
+
+        res.json(result)
+    } catch (err) {
+        console.log('err in saldkfj', err)
+        res.status(404).json({ message: 'somethings wrong' })
+    }
 });
 
 const evaluateResult = async (problemId: Number, code: string, lang: string) => {
@@ -43,16 +59,39 @@ const evaluateResult = async (problemId: Number, code: string, lang: string) => 
         const isCorrect = results.every(result => result.isCorrect)
         const passedCount = results.reduce((acc, res) => acc + (res.isCorrect ? 1 : 0), 0)
         const totalCount = results.length
+
+        // save submission in db
         return {
             results,
             passedCount,
             totalCount,
             isCorrect
         }
+
     } catch (err) {
         console.log(err)
         return { message: 'somethings wrong' }
     }
+}
+const saveInDB = async (result: Result, req: Request) => {
+    return new Promise<void>(async (resolve, reject) => {
+        // console.log(req.user)
+        const submission = await prisma.submission.create({
+            data: {
+                code: req.body.code,
+                lang: req.body.lang,
+                user_id: req.user.user_id,
+                total_tests: result.totalCount,
+                tests_passed: result.passedCount,
+                marks: result.totalCount === result.passedCount ? 10 : 0,
+                examId: req.body.examId,
+                problemId: req.body.problemId,
+                tests: result.results
+            },
+        })
+        // console.log('saved in db', submission)
+        resolve()
+    })
 }
 
 const verifyOutput = (expectedOutput: string, actualOutput: string) => {
