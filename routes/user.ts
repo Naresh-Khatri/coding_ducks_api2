4240b873a6807059d91ad3b3d6e936f1419b6556
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-19 13:29:13 +0530

    add new route POST /users/uploadProfilePicture to change profile picture

--- routes/user.ts
+++ routes/user.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import fileUpload from "express-fileupload";
 import * as userController from "../controllers/user";
 import { checkIfAuthenticated } from "../middlewares/auth-middleware";
 
 const router = Router();
+router.use(fileUpload());
 
 router.get('/', userController.getUsers);
 router.get('/:gid', userController.getUser);
@@ -11,6 +13,7 @@ router.get('/username/:username', userController.getUserUsingUsername);
 router.patch('/', [checkIfAuthenticated], userController.updateUser);
 router.post('/checkUsername', userController.checkUsername);
 router.post('/', userController.createUser);
+router.post('/uploadProfilePicture', [checkIfAuthenticated], userController.uploadProfilePicture);
 router.post('/follow', [checkIfAuthenticated], userController.followUser);
 // router.get('/getFollowDetails/:username', [checkIfAuthenticated], userController.getFollowDetails);
 router.post('/unfollow', [checkIfAuthenticated], userController.unfollowUser);
