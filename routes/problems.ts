e4ff77abbfab9c86a4c6139c0dcf79590414deee
naresh-khatri <naresh.khatri2345@gmail.com>
2023-02-03 16:51:12 +0530

    create new route to fetch  users last submitted code

--- routes/problems.ts
+++ routes/problems.ts
@@ -9,6 +9,7 @@ router.get('/', [checkIfAuthenticated], problemsController.getAllProblems)
 router.get('/examProblems/:examId', problemsController.getExamProblems)
 router.get('/:problemId', [checkIfAuthenticated], problemsController.getProblem)
 router.post('/', [checkIfAuthenticated], problemsController.createProblem)
+router.get('/:problemId/getLastSubmission', [checkIfAuthenticated], problemsController.getLastSubmission)
 router.patch('/:problemId', [checkIfAuthenticated], problemsController.updateProblem)
 router.delete('/:problemId', [checkIfAuthenticated], problemsController.deleteProblem)
 
