11bab88b5f06045a7eebb6866fc1c198c1452692
naresh-khatri <naresh.khatri2345@gmail.com>
2023-03-01 16:51:12 +0530

    add cors middleware and setup socket.io

--- index.ts
+++ index.ts
@@ -1,42 +1,42 @@
-import express, { Request, Response } from "express";
-const app = express();
+import { createServer } from "http";
+import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import { Server } from "socket.io";
 import usersRoute from "./routes/user";
 import filesRoute from "./routes/files";
-import codeRoute from './routes/runCode'
-import examRoute from './routes/exams'
-import problemsRoute from './routes/problems'
-import playgroundRoute from './routes/playground'
-import submissionsRoute from './routes/submissions'
+import codeRoute from "./routes/runCode";
+import examRoute from "./routes/exams";
+import problemsRoute from "./routes/problems";
+import playgroundRoute from "./routes/playground";
+import submissionsRoute from "./routes/submissions";
+import allowCORS from "./middlewares/allow-cors";
+import { setupSocketIO } from "./socketio";
 
 const PORT = process.env.PORT || 3333;
 
-app.use((req: Request, res: Response, next: any) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, Content-Type, X-Auth-Token"
-    );
-    next();
-});
+const app = express();
+
+app.use(allowCORS);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({ origin: true, credentials: true }));
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+setupSocketIO(io);
 
 app.use("/users", usersRoute);
 app.use("/files", filesRoute);
-app.use('/runCode', codeRoute)
-app.use('/exams', examRoute)
-app.use('/problems', problemsRoute)
-app.use('/playground', playgroundRoute)
-app.use('/submissions', submissionsRoute)
+app.use("/runCode", codeRoute);
+app.use("/exams", examRoute);
+app.use("/problems", problemsRoute);
+app.use("/playground", playgroundRoute);
+app.use("/submissions", submissionsRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server up on http://localhost:${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server up on http://localhost:${PORT}`);
 });
