ecb594d2b0e9c44aefe9a4535e35c23bb7e74459
naresh-khatri <naresh.khatri2345@gmail.com>
2023-03-29 16:51:12 +0530

    hide private test case i/o in response obj

--- routes/runCode.ts
+++ routes/runCode.ts
@@ -1,117 +1,149 @@
 import { Request, Response, Router } from "express";
 import { cpp, c, python, java, node } from "compile-run";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import { checkIfAuthenticated } from "../middlewares/auth-middleware";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const router = Router();
 
 interface Result {
-    results: Array<{ input: string, output: string, actualOutput: string, result: any, isCorrect: boolean }>,
-    passedCount: number,
-    totalCount: number,
-    isCorrect: boolean
+  results: Array<{
+    input: string;
+    output: string;
+    actualOutput: string;
+    result: any;
+    isCorrect: boolean;
+  }>;
+  passedCount: number;
+  totalCount: number;
+  isCorrect: boolean;
 }
 
-router.post('/', [checkIfAuthenticated], async (req: Request, res: Response) => {
+router.post(
+  "/",
+  [checkIfAuthenticated],
+  async (req: Request, res: Response) => {
     const { code, lang, problemId } = req.body;
-    // console.log(req.body)
     try {
+      const result = (await evaluateResult(problemId, code, lang)) as Result;
+      if (req.body.submit) await saveInDB(result, req);
 
-
-        const result = await evaluateResult(problemId, code, lang) as Result
-        if (req.body.submit)
-            await saveInDB(result, req)
-
-        res.json(result)
+      res.json(result);
     } catch (err) {
-        console.log('err in saldkfj', err)
-        res.status(404).json({ message: 'somethings wrong' })
+      console.log("err in saldkfj", err);
+      res.status(404).json({ message: "somethings wrong" });
     }
-});
+  }
+);
 
-const evaluateResult = async (problemId: Number, code: string, lang: string) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const problem = await prisma.problem.findUnique({
-                where: { id: +problemId },
-                // select: { testCases: true }
-            })
-            if (!problem?.testCases) return reject({ message: 'problem not found' })
-            //idk why but we need to cast it to an Array
-            const testCases = problem.testCases as Array<{ input: string, output: string }>
-            const results = await Promise.all(testCases.map(async (testCase) => {
-                const { input, output } = testCase
-                // add \n at the end to avoid \r 
-                const result = await runCode(code, lang, input.replaceAll('\\n', '\n') + '\n')
-
-
-                const { stderr } = result
-                if (stderr) return { errorOccurred: true, errorMessage: stderr }
-                return {
-                    input,
-                    output,
-                    actualOutput: result.stdout,
-                    result,
-                    isCorrect: verifyOutput(output, result.stdout)
-                }
-            }))
-            const isCorrect = results.every(result => result.isCorrect)
-            const passedCount = results.reduce((acc, res) => acc + (res.isCorrect ? 1 : 0), 0)
-            const totalCount = results.length
-
-            resolve({
-                results,
-                passedCount,
-                totalCount,
-                isCorrect
-            })
-
-        } catch (err) {
-            console.log(err)
-            reject({ message: 'somethings wrong' })
-        }
-    })
+interface Testcase {
+  input: string;
+  output: string;
+  isPublic: boolean;
 }
-const saveInDB = async (result: Result, req: Request) => {
-    return new Promise<void>(async (resolve, reject) => {
-        const submission = await prisma.submission.create({
-            data: {
-                code: req.body.code,
-                lang: req.body.lang,
-                userId: req.user.userId,
-                total_tests: result.totalCount,
-                tests_passed: result.passedCount,
-                marks: result.totalCount === result.passedCount ? 10 : 0,
-                examId: req.body.examId,
-                problemId: req.body.problemId,
-                tests: result.results
-            },
+
+const evaluateResult = async (
+  problemId: Number,
+  code: string,
+  lang: string
+) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const problem = await prisma.problem.findUnique({
+        where: { id: +problemId },
+      });
+      if (!problem?.testCases) return reject({ message: "problem not found" });
+      //idk why but we need to cast it to an Array
+      const testCases = problem.testCases as Array<{
+        input: string;
+        output: string;
+        isPublic: boolean;
+        explanation?: string;
+      }>;
+      const results = await Promise.all(
+        testCases.map(async (testCase) => {
+          const { input, output } = testCase;
+          // add \n at the end to avoid \r
+          const result = await runCode(
+            code,
+            lang,
+            input.replaceAll("\\n", "\n") + "\n"
+          );
+          const { stderr } = result;
+          if (stderr) return { errorOccurred: true, errorMessage: stderr };
+
+          if (testCase.isPublic)
+            return {
+              input,
+              output,
+              actualOutput: result.stdout,
+              result,
+              isHidden: false,
+              isCorrect: verifyOutput(output, result.stdout),
+            };
+          else
+            return {
+              result,
+              isHidden: true,
+              isCorrect: verifyOutput(output, result.stdout),
+            };
         })
-        // console.log('saved in db', submission)
-        resolve()
-    })
-}
+      );
+      const isCorrect = results.every((result) => result.isCorrect);
+      const passedCount = results.reduce(
+        (acc, res) => acc + (res.isCorrect ? 1 : 0),
+        0
+      );
+      const totalCount = results.length;
+
+      resolve({
+        results,
+        passedCount,
+        totalCount,
+        isCorrect,
+      });
+    } catch (err) {
+      console.log(err);
+      reject({ message: "somethings wrong" });
+    }
+  });
+};
+const saveInDB = async (result: Result, req: Request) => {
+  return new Promise<void>(async (resolve, reject) => {
+    const submission = await prisma.submission.create({
+      data: {
+        code: req.body.code,
+        lang: req.body.lang,
+        userId: req.user.userId,
+        total_tests: result.totalCount,
+        tests_passed: result.passedCount,
+        marks: result.totalCount === result.passedCount ? 10 : 0,
+        examId: req.body.examId,
+        problemId: req.body.problemId,
+        tests: result.results,
+      },
+    });
+    // console.log('saved in db', submission)
+    resolve();
+  });
+};
 
 const verifyOutput = (expectedOutput: string, actualOutput: string) => {
-    // console.log(expectedOutput.trim().replaceAll(/\t|\n|\r| /g,""), actualOutput.trim().replaceAll(/\t|\n|\r| /g,""))
-    return expectedOutput.trim().replaceAll(/\t|\n|\r| /g, "") === actualOutput.trim().replaceAll(/\t|\n|\r| /g, "")
-}
+  // console.log(expectedOutput.trim().replaceAll(/\t|\n|\r| /g,""), actualOutput.trim().replaceAll(/\t|\n|\r| /g,""))
+  return (
+    expectedOutput.trim().replaceAll(/\t|\n|\r| /g, "") ===
+    actualOutput.trim().replaceAll(/\t|\n|\r| /g, "")
+  );
+};
 
 const runCode = async (code: string, lang: string, input: string) => {
-    // console.log("input", input)
-    if (lang === 'c')
-        return c.runSource(code, { stdin: input })
-    if (lang === 'cpp')
-        return cpp.runSource(code, { stdin: input })
-    if (lang === 'py')
-        return python.runSource(code, { stdin: input })
-    if (lang === 'java')
-        return java.runSource(code, { stdin: input })
-    if (lang === 'js')
-        return node.runSource(code, { stdin: input })
-    return python.runSource(code, { stdin: input })
-}
+  // console.log("input", input)
+  if (lang === "c") return c.runSource(code, { stdin: input });
+  if (lang === "cpp") return cpp.runSource(code, { stdin: input });
+  if (lang === "py") return python.runSource(code, { stdin: input });
+  if (lang === "java") return java.runSource(code, { stdin: input });
+  if (lang === "js") return node.runSource(code, { stdin: input });
+  return python.runSource(code, { stdin: input });
+};
 
-
-export default router
+export default router;
