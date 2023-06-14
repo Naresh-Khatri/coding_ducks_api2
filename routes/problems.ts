ce6d00464b9104197881d67d2a92d4da384844fe
naresh-khatri <naresh.khatri2345@gmail.com>
2023-06-14 16:55:12 +0530

    feat: add GET /pages and /tags endpoints

--- routes/problems.ts
+++ routes/problems.ts
@@ -6,6 +6,8 @@ const router = Router()
 
 //TODO: add auth
 router.get('/', [checkIfAuthenticated], problemsController.getAllProblems)
+router.get('/page', problemsController.getProblemsForProblemPage)
+router.get('/tags', problemsController.getProblemTags)
 router.get('/examProblems/:examId',[checkIfAuthenticated], problemsController.getExamProblems)
 router.get('/:problemId', [checkIfAuthenticated], problemsController.getProblem)
 router.post('/', [checkIfAuthenticated], problemsController.createProblem)
