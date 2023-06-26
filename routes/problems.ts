6b3c272ba45604a211c45c1c5cbdf0528388c9e9
naresh-khatri <naresh.khatri2345@gmail.com>
2023-06-26 16:55:12 +0530

    feat: :stars: add new routes to get problems and their ratings

--- routes/problems.ts
+++ routes/problems.ts
@@ -1,19 +1,28 @@
 import { Router } from 'express'
 import * as problemsController from '../controllers/problems'
-import { checkIfAuthenticated } from '../middlewares/auth-middleware'
+import { checkIfAdmin, checkIfAuthenticated } from '../middlewares/auth-middleware'
 
 const router = Router()
 
 //TODO: add auth
-router.get('/', [checkIfAuthenticated], problemsController.getAllProblems)
-router.get('/page', problemsController.getProblemsForProblemPage)
-router.get('/tags', problemsController.getProblemTags)
-router.get('/examProblems/:examId',[checkIfAuthenticated], problemsController.getExamProblems)
-router.get('/:problemId', [checkIfAuthenticated], problemsController.getProblem)
+router.get('/', [checkIfAuthenticated, checkIfAdmin], problemsController.getAllProblems)
 router.post('/', [checkIfAuthenticated], problemsController.createProblem)
+
+router.get('/tags', problemsController.getProblemTags)
+
+router.get('/page', problemsController.getProblemsForProblemPage)
+router.get('/slug/:slug', problemsController.getProblemBySlug)
+router.get('/:problemId', [checkIfAuthenticated], problemsController.getProblem)
+router.get('/examProblems/:examId',[checkIfAuthenticated], problemsController.getExamProblems)
+
+router.get('/:problemId/ratings/', [], problemsController.getProblemRating)
+router.patch('/:problemId/ratings/', [checkIfAuthenticated], problemsController.updateProblemRating)
+
 router.get('/:problemId/getLastSubmission', [checkIfAuthenticated], problemsController.getLastSubmission)
+
 router.patch('/:problemId', [checkIfAuthenticated], problemsController.updateProblem)
 router.delete('/:problemId', [checkIfAuthenticated], problemsController.deleteProblem)
+
 
 export default router
 
