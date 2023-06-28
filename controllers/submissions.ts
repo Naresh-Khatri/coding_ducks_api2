3512feccc972ffa21633567e2e56a05ba84920eb
naresh-khatri <naresh.khatri2345@gmail.com>
2023-06-28 16:55:12 +0530

    feat: :stars: create controllers for new routes

--- controllers/submissions.ts
+++ controllers/submissions.ts
@@ -96,35 +96,103 @@ export const getSubmissionById = async (req: Request, res: Response) => {
       where: {
         id: +req.params.submissionId,
       },
-    });
-    const user = await prisma.user.findFirst({
-      where: {
-        id: submission?.userId,
-      },
-      select: {
-        fullname: true,
-        username: true,
-        photoURL: true,
-        roll: true,
-      },
-    });
-    const exam = await prisma.exam.findFirst({
-      where: {
-        id: submission?.examId,
-      },
-      select: {
-        id: true,
-        slug: true,
-        title: true,
+      include: {
+        User: {
+          select: {
+            fullname: true,
+            username: true,
+            photoURL: true,
+            roll: true,
+          },
+        },
+        Exam: {
+          select: {
+            id: true,
+            slug: true,
+            title: true,
+          },
+        },
+        Problem: true,
       },
     });
-    const problem = await prisma.problem.findFirst({
-      where: {
-        id: submission?.problemId,
-      },
-    });
+    if (!submission)
+      return res.status(404).json({ message: "submission not found" });
 
-    res.status(200).json({ submission, user, exam, problem });
+    res.status(200).json({ data: submission, message: "success" });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ message: "somethings wrong" });
+  }
+};
+
+interface IRequest extends Request {
+  query: {
+    problemId: string;
+    lang: string;
+  };
+}
+export const getLastSubmission = async (req: IRequest, res: Response) => {
+  try {
+    const { problemId, lang } = req.query;
+    if (!problemId || !lang)
+      return res.status(400).json({ message: "params not sent" });
+
+    const sub = await prisma.submission.findFirst({
+      where: {
+        problemId: +problemId,
+        lang,
+        marks: 10,
+      },
+      orderBy: {
+        timestamp: "desc",
+      },
+    });
+    if (!sub)
+      return res.status(404).json({
+        data: null,
+        message: "Submission not found for " + problemId + " " + lang,
+      });
+    res.status(200).json({ data: sub, message: "success" });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ message: "somethings wrong" });
+  }
+};
+
+export const getSubmissionsByCurrentUserAndProblemId = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { problemId } = req.params;
+    const { userId } = req.user;
+    if (!problemId || !userId)
+      return res.status(400).json({ message: "params not sent" });
+
+    const sub = await prisma.submission.findMany({
+      where: {
+        problemId: +problemId,
+        userId: userId,
+      },
+      include: {
+        User: {
+          select: {
+            fullname: true,
+            username: true,
+            photoURL: true,
+          },
+        },
+      },
+      orderBy: {
+        timestamp: "desc",
+      },
+    });
+    if (!sub)
+      return res.status(404).json({
+        data: null,
+        message: "Submission not found for " + problemId + " " + userId,
+      });
+    res.status(200).json({ data: sub, message: "success" });
   } catch (err) {
     console.log(err);
     res.status(404).json({ message: "somethings wrong" });
