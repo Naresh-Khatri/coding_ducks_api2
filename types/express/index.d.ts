3a0a3841c30838ba37e9219a29dea025df5366dd
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-11 16:55:12 +0530

    feat: create new property 'isNoob'

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -2,6 +2,7 @@ import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 interface IUser extends DecodedIdToken {
   userId: number;
   isAdmin: boolean;
+  isNoob: boolean;
   uid: string;
 }
 
