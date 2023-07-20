9b375b52c8758319dc064f274b0a4c278f7e8988
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-20 23:35:13 +0530

    feat: :truck: rename /progress to /stats

--- routes/user.ts
+++ routes/user.ts
@@ -8,7 +8,7 @@ router.use(fileUpload());
 
 router.get('/', userController.getUsers);
 router.get('/:gid', userController.getUser);
-router.get('/username/:username/progress', userController.getUserProgress);
+router.get('/username/:username/stats', userController.getUserStats);
 router.get('/username/:username', userController.getUserUsingUsername);
 router.patch('/', [checkIfAuthenticated], userController.updateUser);
 router.post('/checkUsername', userController.checkUsername);
