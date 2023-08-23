98876b77a3bd2e256d11fcdb724140c4d4e1b726
naresh-khatri <naresh.khatri2345@gmail.com>
2023-08-23 16:55:12 +0530

    feat: replace compile-run with turbodrive

--- routes/playground.ts
+++ routes/playground.ts
@@ -1,8 +1,9 @@
 import { Request, Response, Router } from "express";
-import { cpp, c, python, java, node } from "compile-run";
 import { checkIfAuthenticated } from "../middlewares/auth-middleware";
 
 import { PrismaClient } from "@prisma/client";
+import { Lang } from "../types";
+import { execCode } from "../turbodrive";
 const prisma = new PrismaClient();
 
 const router = Router();
@@ -12,10 +13,24 @@ router.post(
   [checkIfAuthenticated],
   async (req: Request, res: Response) => {
     const { code, lang } = req.body;
-    const input = req.body.input || "";
+    const inputs = req.body.inputs || [""];
     try {
-      const result = await runCode(code, lang, input);
-      res.json(result);
+      // Deterministically run garbage collector
+      if (global.gc) global.gc();
+
+      const startMemory = process.memoryUsage().heapUsed;
+      const result = await execCode({
+        lang,
+        code,
+        inputs,
+        options: { maxBuffer: 1024 * 1024, timeout:3000 },
+      });
+      res.json({
+        ...result,
+        memory: ((process.memoryUsage().heapUsed - startMemory) / 1024).toFixed(
+          2
+        ),
+      });
     } catch (err) {
       console.log("err in saldkfj", err);
       res.status(404).json({ message: "somethings wrong" });
@@ -32,14 +47,14 @@ router.get("/rooms", async (req: Request, res: Response) => {
   }
 });
 
-export const runCode = async (code: string, lang: string, input: string) => {
+export const runCode = async (code: string, lang: Lang, inputs: string[]) => {
+  return execCode({
+    lang,
+    code,
+    inputs,
+    options: { maxBuffer: 1024 * 1024 },
+  });
   // console.log("input", input)
-  if (lang === "c") return c.runSource(code, { stdin: input });
-  if (lang === "cpp") return cpp.runSource(code, { stdin: input });
-  if (lang === "py") return python.runSource(code, { stdin: input });
-  if (lang === "java") return java.runSource(code, { stdin: input });
-  if (lang === "js") return node.runSource(code, { stdin: input });
-  return python.runSource(code, { stdin: input });
 };
 
 export default router;
