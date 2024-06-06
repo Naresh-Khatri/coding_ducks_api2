03687894966c950c81fda108b40210ee05650287
naresh-khatri <naresh.khatri2345@gmail.com>
2024-06-06 10:53:26 +0530

    feat: :lock: avoid checking auth on /playground

--- routes/playground.ts
+++ routes/playground.ts
@@ -7,35 +7,31 @@ import prisma from "../lib/prisma";
 
 const router = Router();
 
-router.post(
-  "/",
-  [checkIfAuthenticated],
-  async (req: Request, res: Response) => {
-    const { code, lang } = req.body;
-    const inputs = req.body.inputs || [""];
-    try {
-      // Deterministically run garbage collector
-      if (global.gc) global.gc();
+router.post("/", async (req: Request, res: Response) => {
+  const { code, lang } = req.body;
+  const inputs = req.body.inputs || [""];
+  try {
+    // Deterministically run garbage collector
+    if (global.gc) global.gc();
 
-      const startMemory = process.memoryUsage().heapUsed;
-      const result = await execCode({
-        lang,
-        code,
-        inputs,
-        options: { maxBuffer: 1024 * 1024, timeout: 3000 },
-      });
-      res.json({
-        ...result,
-        memory: ((process.memoryUsage().heapUsed - startMemory) / 1024).toFixed(
-          2
-        ),
-      });
-    } catch (err) {
-      console.log("err in saldkfj", err);
-      res.status(404).json({ message: "somethings wrong" });
-    }
+    const startMemory = process.memoryUsage().heapUsed;
+    const result = await execCode({
+      lang,
+      code,
+      inputs,
+      options: { maxBuffer: 1024 * 1024, timeout: 3000 },
+    });
+    res.json({
+      ...result,
+      memory: ((process.memoryUsage().heapUsed - startMemory) / 1024).toFixed(
+        2
+      ),
+    });
+  } catch (err) {
+    console.log("err in saldkfj", err);
+    res.status(404).json({ message: "somethings wrong" });
   }
-);
+});
 router.get("/rooms", async (req: Request, res: Response) => {
   try {
     const rooms = await prisma.room.findMany();
