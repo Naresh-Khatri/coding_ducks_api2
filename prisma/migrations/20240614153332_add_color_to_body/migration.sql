cb7212db5f064ce47f65a0750493e0dd89d01b29
naresh-khatri <naresh.khatri2345@gmail.com>
2024-06-14 21:04:22 +0530

    fix: :lipstick: set attempts default body background color to white

---
+++ prisma/migrations/20240614153332_add_color_to_body/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "ChallengeAttempt" ALTER COLUMN "contentCSS" SET DEFAULT 'body{
+  margin: 0px;
+  height: 100dvh;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}';
